d77b2500909bb5cc741f1d84878b83803e3da671
ZhichuanLi <lizhichuan2012@gmail.com>
2019-02-01 15:22:13 +0100

    Update testfile.py
    adding code for check deeplearning library version info

--- testfile.py
+++ testfile.py
@@ -1,5 +1,17 @@
-import theano
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 15:19:53 2019
+
+@author: lizhichuan
+"""
+#import theano
 import tensorflow
 import keras
 
-print ("Test")
+print('Hello, Anaconda for deep learning!')
+#theano
+#print('theano: %s' % theano.__version__)
+# tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+print('keras: %s' % keras.__version__)
