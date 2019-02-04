3546c071a235dbb13eef74d97bf35c2dd8790b3b
ZhichuanLi <lizhichuan2012@gmail.com>
2019-02-04 10:57:47 +0100

    Upload cnn.py
    cnn.py file is provided by Pong
    
    Co-Authored-By: Pongsakorn Chanchaipol <p.chanchaipol@gmail.com>

--- MNIST.py
+++ MNIST.py
@@ -44,7 +44,8 @@ from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 model = Sequential()
 model.add(Conv2D(28, kernel_size=(3,3), input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten()) # Flattening the 2D arrays for fully connected layers
+# Flattening the 2D arrays for fully connected layers
+model.add(Flatten())                        
 model.add(Dense(128, activation=tf.nn.relu))
 model.add(Dropout(0.2))
 model.add(Dense(10,activation=tf.nn.softmax))
