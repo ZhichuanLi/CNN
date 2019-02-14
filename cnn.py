dcfefbafb336f79325d2b2fe4ab092c9a89aeefd
Pongsakorn Chanchaipol <p.chanchaipol@gmail.com>
2019-02-14 11:42:30 +0100

    Update cnn.py
    change some dimension parameter and make it trainable on 5 classes dataset

--- cnn.py
+++ cnn.py
@@ -1,14 +1,3 @@
-# Convolutional Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
@@ -36,10 +25,10 @@ classifier.add(Flatten())
 
 # Step 4 - Full connection
 classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(output_dim = 5, activation = 'sigmoid'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
 
@@ -52,18 +41,19 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory(r'dataset\training',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
-                                                 class_mode = 'binary')
+                                                 batch_size = 64,
+                                                 class_mode = 'categorical')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory(r'dataset\testing',
                                             target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
+                                            batch_size = 64,
+                                            class_mode = 'categorical')
 
 classifier.fit_generator(training_set,
-                         samples_per_epoch = 8000,
-                         nb_epoch = 25,
+                         samples_per_epoch = 2589,
+                         nb_epoch = 10,
                          validation_data = test_set,
-                         nb_val_samples = 2000)
+                         nb_val_samples = 868)
+
